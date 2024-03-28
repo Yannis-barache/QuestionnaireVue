efbac391c8d1731ddbc736e85702a1ea378be3e5
khalil-45 <khalil.abada@etu.univ-orleans.fr>
2024-03-28 22:10:04 +0100

    affichage des quizs + clic sur quiz alors infos quiz affiché

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,16 +1,39 @@
 <script setup>
 import { ref, onMounted } from 'vue'
+import ProviderQuiz from '../Provider/ProviderQuiz.js'
 
+const questionnaires = ref([])
+const selectedQuiz = ref(null)
+
+onMounted(async () => {
+  questionnaires.value = await ProviderQuiz.getAllQuiz()
+})
+
+const selectQuiz = (quiz) => {
+  selectedQuiz.value = quiz
+}
+
+defineExpose({
+  questionnaires,
+  selectedQuiz,
+  selectQuiz
+})
 </script>
 
 <template>
   <div>
     <h1>Questionnaires</h1>
-    <div v-for="questionnaire in questionnaires" :key="questionnaire.text">
-      <h2>{{ questionnaire.text }}</h2>
-      <div v-for="question in questionnaire.getQuestions()" :key="question.getId()">
-        <p>{{ question.getQuestion() }}</p>
-      </div>
+    <div v-for="questionnaire in questionnaires" :key="questionnaire.quiz_id" @click="selectQuiz(questionnaire)">
+      <h2>{{ questionnaire.name }}</h2>
+      <ul>
+        <li v-for="(question, index) in questionnaire.questions" :key="index">
+          {{ question }}
+        </li>
+      </ul>
+    </div>
+    <div v-if="selectedQuiz">
+      <h2>Selected Quiz: {{ selectedQuiz.name }}</h2>
+      <!-- INFORMATIONS SUR LE QUIZ SELECTIONNÉ -->
     </div>
   </div>
 </template>
