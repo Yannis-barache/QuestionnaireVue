8c287697a534988bd6a13f74337bca2f07aeca09
khalil-45 <khalil.abada@etu.univ-orleans.fr>
2024-03-28 09:53:44 +0100

    suppression de fichiers inutile

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,21 +1,4 @@
 <script setup>
-import { ref } from 'vue'
-import Questionnaire from "@/components/Questionnaire.js";
-
-const Questionnaires = [
-    new Questionnaire("Questionnaire 1"),
-    new Questionnaire("Questionnaire 2"),
-    new Questionnaire("Questionnaire 3"),
-]
-
-for (let i = 0; i < Questionnaires.length; i++) {
-    Questionnaires[i].addQuestion("Question 1")
-    Questionnaires[i].addQuestion("Question 2")
-    Questionnaires[i].addQuestion("Question 3")
-}
-
-const questionnaires = ref(Questionnaires)
-
 
 </script>
 
