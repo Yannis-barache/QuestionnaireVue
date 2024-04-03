d380bf6442b96191cdcec605a6a27e1082ad9f02
Yannis Barache <112858061+Yannis-barache@users.noreply.github.com>
2024-04-03 18:28:21 +0200

    Modif setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
 virtualenv -p python3 venv
 source venv/bin/activate
 pip install -r requirements.txt
+echo "FLASK_APP=application" > .flaskenv
+echo "FLASK_DEBUG= TRUE" >> .flaskenv
+
+echo "Setup complete. Lancez 'source venv/bin/activate' pour activer l'environnement virtuel."
